2221843ade9296c4137fb96b48f344ebaa7fba8c
unknown <Dmitiy_yar@mail.ru>
2022-09-14 09:02:14 +0300

    task1

---
+++ Task1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+int size = 5;
+int[] arrey = GetArray(size);
+int[] GetArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+    return arr;
+}
+
+Console.WriteLine($"[ {String.Join("\t", arrey)} ]");
+Console.WriteLine(sortArrey(arrey, size));
+
+int sortArrey(int[] arrey, int size)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        if (arrey[i] % 2 == 0)
+            count++;
+    }
+    return count;
+}
